da4d849a32a5b0e07c47038dc4d1b12f54449ee7
TequilaJohnny <65027849+TequilaJohnny@users.noreply.github.com>
2024-09-13 20:34:56 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://notesapp-frontend-production.up.railway.app/',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
